9fd3f2d5d44e1b1db85b1cd181cb9701ff5e065e
kabagouda <technodor7@gmail.com>
2025-01-17 18:37:24 +0000

    Add animations to main components and remove unused Stats component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -8,20 +8,34 @@ import Services from "@/components/services";
 import WhatsAppButton from "@/components/whatsapp-button";
 import WhyUs from "@/components/why-us";
 import WorkProcess from "@/components/work-process";
+import { Bounce, Fade, Slide, Zoom } from "react-awesome-reveal";
 
 export default function Home() {
   return (
-    <div className="flex flex-col min-h-screen">
+    <div className="flex flex-col min-h-screen overflow-x-hidden">
       <Header />
       <main className="flex-grow">
-        <Hero />
-        <Services />
-        <WhyUs />
-        <RecentProjects />
-        <WorkProcess />
-        {/* <Stats /> */}
-        <ContactForm />
-        <BookConsultation />
+        <Fade>
+          <Hero />
+        </Fade>
+        <Slide>
+          <Services />
+        </Slide>
+        <Bounce>
+          <WhyUs />
+        </Bounce>
+        <Zoom>
+          <RecentProjects />
+        </Zoom>
+        <Slide>
+          <WorkProcess />
+        </Slide>
+        <Fade>
+          <ContactForm />
+        </Fade>
+        <Bounce>
+          <BookConsultation />
+        </Bounce>
       </main>
       <Footer />
       <WhatsAppButton />
