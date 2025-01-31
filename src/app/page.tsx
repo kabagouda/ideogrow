7b726796adc8a41bfe6a4435143fb944cafd38dd
kabagouda <technodor7@gmail.com>
2025-01-31 18:54:13 +0000

    Refactor Home component to remove animation wrappers for improved performance

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -8,34 +8,19 @@ import Services from "@/components/services";
 import WhatsAppButton from "@/components/whatsapp-button";
 import WhyUs from "@/components/why-us";
 import WorkProcess from "@/components/work-process";
-import { Bounce, Fade, Slide, Zoom } from "react-awesome-reveal";
 
 export default function Home() {
   return (
     <div className="flex flex-col min-h-screen overflow-x-hidden">
       <Header />
       <main className="flex-grow">
-        <Fade>
-          <Hero />
-        </Fade>
-        <Slide>
-          <Services />
-        </Slide>
-        <Bounce>
-          <WhyUs />
-        </Bounce>
-        <Zoom>
-          <RecentProjects />
-        </Zoom>
-        <Slide>
-          <WorkProcess />
-        </Slide>
-        <Fade>
-          <ContactForm />
-        </Fade>
-        <Bounce>
-          <BookConsultation />
-        </Bounce>
+        <Hero />
+        <Services />
+        <WhyUs />
+        <RecentProjects />
+        <WorkProcess />
+        <ContactForm />
+        <BookConsultation />
       </main>
       <Footer />
       <WhatsAppButton />
